b93a7f801067919acf5bb3b85692061bbd624c5b
eugenio-perdomo <scnhp.0894@gmail.com>
2021-05-29 19:53:03 -0300

    Merge entre lo anterior y lo nuevo

---
+++ resources/views/jugadores/lista.blade.php
@@ -0,0 +1,53 @@
+<!-- Styles -->
+
+<head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+</head>
+<html>
+@include('layouts/master')
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200 pb-5">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card mt-4 shadow-lg">
+                                <div class="card-header d-flex justify-content-between align-items-center">
+                                    <h3>Lista de Jugadores</h3>
+                                    <a href="/jugadores/create" class="btn btn-primary btn-sm">Nuevo Jugador</a>
+                                </div>
+                                <div class="card-body">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Nombre</th>
+                                                <th scope="col">Apellido</th>
+                                                <th scope="col">Nacimiento</th>
+                                                <th scope="col">Nacionalidad</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($jugadores as $jugador)
+                                            <tr>
+                                                <th scope="row">{{ $jugador->nombre }}</th>
+                                                <td>{{ $jugador->apellido }}</td>
+                                                <td>{{ $jugador->fNacimiento }}</td>
+                                                <td>{{ $jugador->nacionalidad }}</td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</html>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!---Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Master Page</title>
+</head>
+
+<body class="antialiased">
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-lg" style="background-color: #002766;">
+        <div class="container-fluid">
+            <a class="navbar-brand text-light pe-4 border-end" href="">
+                <h3>Diario Deportivo</h3>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-light" aria-current="page" href="#">Jugadores</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Equipos</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Administración
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Crear Notas</a></li>
+                            <li><a class="dropdown-item" href="#">Modificar Notas</a></li>
+                            <li><a class="dropdown-item" href="#">Eliminar Notas</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Solicitudes de Administrador</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="/jugadores">Crear Jugador</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Buscar</button>
+                </form>
+                @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                    
+                    <li class="nav-item dropdown btn">
+                        <a class="nav-link dropdown-toggle btn-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{Auth::user()->name}}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link class="text-decoration-none" :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log out') }}
+                            </x-dropdown-link>
+                        </form></li>
+                        </ul>
+                    </li>
+                    @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline btn btn-light ms-5">Log in</a>
+
+                    @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline btn btn-light ms-1">Registro</a>
+                    @endif
+                    @endauth
+                </div>
+                @endif
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+            </div>
+        </div>
+    </nav>
+
+    <!---Bootstrap Script-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
