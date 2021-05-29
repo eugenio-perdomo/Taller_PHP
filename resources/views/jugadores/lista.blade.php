892352e254460cc880072e9fe0996ef08cef3aec
eugenio-perdomo <scnhp.0894@gmail.com>
2021-05-29 20:56:43 -0300

    Cambios al route/web

--- resources/views/jugadores/lista.blade.php
+++ resources/views/jugadores/lista.blade.php
@@ -1,11 +1,14 @@
-<!-- Styles -->
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
-<html>
+<body>
 @include('layouts/master')
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -49,5 +52,5 @@
         </div>
     </div>
 </div>
-
+</body>
 </html>
