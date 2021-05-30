79773be196c8751d50a48cbc0dfe61faaf197ec4
pali13 <pali.bolso.13@gmail.com>
2021-05-30 15:32:19 -0300

    Registros de usuarios segúun tipoUsuario

--- app/Models/Administrador.php
+++ app/Models/Administrador.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Administrador extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'administrador_id',
+    ];
+
+    protected $table = 'administrador'; //con esto laravel deja de ir a buscar administradors a la base de datos y va buscar a administrador 
 }
