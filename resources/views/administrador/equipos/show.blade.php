376b0da74fafbffbc4432b9780e19c168c3cf042
cmgpWLapTop <cmgpguerracarlos@gmail.com>
2021-06-08 04:46:31 -0300

    se agrega vista show jugadores  ligas y equipos

--- resources/views/administrador/equipos/show.blade.php
+++ resources/views/administrador/equipos/show.blade.php
@@ -1,1 +1,44 @@
-<h1>en el show</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+</head>
+<body>
+@include('layouts/app')
+<section class="d-flex flex-column align-items-start">
+    <div class="p-3 mt-3">
+        <h1>  {{ $equipo->nombre }}</h1>
+    </div>
+    
+    <div class="p-4 mb-3">
+        <div class="">
+            <div class="">
+                <strong>Name:</strong>
+                {{ $equipo->nombre  }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nombre Corto:</strong>
+                {{ $equipo->nomCorto  }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tres Letras:</strong>
+                {{ $equipo->tresLetras  }}
+            </div>
+        </div>
+    </div>
+    <div class="p-3">
+        <a class="btn btn-primary" href="{{ route('equipos.index') }}" title="Go back"> Regresar </a>
+    </div>
+</section>
+
+</body>
+</html>
--- resources/views/administrador/ligas/show.blade.php
+++ resources/views/administrador/ligas/show.blade.php
@@ -1,1 +1,44 @@
-<h1>en el show</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+</head>
+<body>
+@include('layouts/app')
+<section class="d-flex flex-column align-items-start">
+    <div class="p-3 mt-3">
+        <h1>  {{ $liga->nombreLiga }}</h1>
+    </div>
+    
+    <div class="p-4 mb-3">
+        <div class="">
+            <div class="">
+                <strong>Nombre:</strong>
+                {{ $liga->nombreLiga  }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Cantidad de participantes:</strong>
+                {{ $liga->participantes  }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Sistema de juego:</strong>
+                {{ $liga->sistemaDeJuego  }}
+            </div>
+        </div>
+    </div>
+    <div class="p-3">
+        <a class="btn btn-primary" href="{{ route('ligas.index') }}" title="Go back"> Regresar </a>
+    </div>
+</section>
+
+</body>
+</html>
