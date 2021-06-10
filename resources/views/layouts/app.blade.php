544e43e91baf1d6b3a054e91371a8f86a6faf7ad
pali13 <pali.bolso.13@gmail.com>
2021-06-10 08:11:03 -0300

    Nuevos permisos y nuevos controles de vistas por roles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 
 <head>
     <!---Bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,7 +18,9 @@
             <a class="navbar-brand text-light pe-4 border-end" href="/">
                 <h3>Diario Deportivo</h3>
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -35,7 +38,8 @@
                         <a class="nav-link text-light" href="/noticia/lista">Noticias</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
                             Administración
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -46,16 +50,26 @@
                                 <hr class="dropdown-divider">
                             </li>
                             @can('roles.index')
-                            <li><a class="dropdown-item" href="{{route('roles.index')}}">Solicitudes de Roles</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
+                                <li><a class="dropdown-item" href="{{ route('roles.index') }}">Solicitudes de Roles</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
                             @endcan
-                            <li><a class="dropdown-item" href="/jugadores/create">Crear Jugador</a></li>
-                            <li><a class="dropdown-item" href="/equipos/create">Crear Equipo</a></li>
-                            <li><a class="dropdown-item" href="/jugadors">Administrar Jugadores</a></li>
-                            <li><a class="dropdown-item" href="/equipos">Administrar Equipos</a></li>
-                            <li><a class="dropdown-item" href="/ligas">Administrar Ligas</a></li>
+                            @can('jugadores.create')
+                                <li><a class="dropdown-item" href="/jugadores/create">Crear Jugador</a></li>
+                            @endcan
+                            @can('equipos.create')
+                                <li><a class="dropdown-item" href="/equipos/create">Crear Equipo</a></li>
+                            @endcan
+                            @can('jugadores.create')
+                                <li><a class="dropdown-item" href="/jugadors">Administrar Jugadores</a></li>
+                            @endcan
+                            @can('equipos.create')
+                                <li><a class="dropdown-item" href="/equipos">Administrar Equipos</a></li>
+                            @endcan
+                            @can('ligas.create')
+                                <li><a class="dropdown-item" href="/ligas">Administrar Ligas</a></li>
+                            @endcan
                         </ul>
                     </li>
                 </ul>
@@ -64,31 +78,36 @@
                     <button class="btn btn-outline-success" type="submit">Buscar</button>
                 </form>
                 @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
+                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                        @auth
 
-                    <li class="nav-item dropdown btn">
-                        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            {{Auth::user()->username}}
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="{{ url('/logout') }}">
-                                    {{ __('Cerrar Sesión') }}</a></li>
-                        </ul>
-                    </li>
-                    </form>
-                    </ul>
-                    </li>
-                    @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline btn btn-light ms-5">Iniciar Sesión</a>
+                            <li class="nav-item dropdown btn">
+                                <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    {{ Auth::user()->username }}
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="{{ url('/logout') }}">
+                                            {{ __('Cerrar Sesión') }}</a></li>
+                                </ul>
+                            </li>
+                            </form>
+                            </ul>
+                            </li>
+                        @else
+                            <a href="{{ route('login') }}"
+                                class="text-sm text-gray-700 underline btn btn-light ms-5">Iniciar Sesión</a>
 
-                    @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline btn btn-light ms-1">Registro</a>
-                    @endif
-                    @endauth
-                </div>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}"
+                                    class="ml-4 text-sm text-gray-700 underline btn btn-light ms-1">Registro</a>
+                            @endif
+                        @endauth
+                    </div>
                 @endif
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+                    crossorigin="anonymous"></script>
             </div>
         </div>
     </nav>
